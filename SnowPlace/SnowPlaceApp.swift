7ee67d75a48f1c6c4198de7a060b365232c72f15
Maksymilian <ts3joker@gmail.com>
2023-02-09 10:51:47 +0100

    Initial Commit

---
+++ SnowPlace/SnowPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowPlaceApp.swift
+//  SnowPlace
+//
+//  Created by Maksymilian Patoń on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
